b75e033139ff1dc5cce5420aac0213916c663673
Csantucci <carlosanit1@gmail.com>
2024-09-11 16:39:25 +0200

    Fix in NextStationWindow

--- Source/Orts.Simulation/Simulation/Simulator.cs
+++ Source/Orts.Simulation/Simulation/Simulator.cs
@@ -2168,6 +2168,8 @@ namespace Orts.Simulation
                 }
                 if (!TimetableMode)
                     (PlayerLocomotive.Train as AITrain).SwitchToPlayerControl();
+                else
+                    PlayerLocomotive.Train.DisplayMessage = "";
             }
             else
             {
