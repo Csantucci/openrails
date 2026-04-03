1cafda75f6097ee5e47b103eb8f66c652b5d6eaa
csantucci <Carlosanit1@gmail.com>
2026-04-03 18:00:30 +0200

    Fix for null crash in timetable mode

--- Source/Orts.Simulation/Simulation/Timetables/TTTrain.cs
+++ Source/Orts.Simulation/Simulation/Timetables/TTTrain.cs
@@ -1534,6 +1534,9 @@ namespace Orts.Simulation.Timetables
                     if (arrivalDT.HasValue)
                     {
                         thisStation.arrivalDT = arrivalDT.Value;
+                    }
+                    if (departureDT.HasValue)
+                    {
                         thisStation.departureDT = departureDT.Value;
                     }
                 }
